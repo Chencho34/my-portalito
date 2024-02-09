8507d06d09c40f84014c8d50614e8382d0519099
Armando <armandocrescencio343@gmail.com>
2024-02-09 10:28:54 -0600

    INSTALL: toast

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,24 @@
+import { Bounce, ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 export default function Home() {
+  const handleToast = () => toast('Welcome Again Armando!', {
+    position: 'bottom-right',
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'dark',
+    transition: Bounce,
+  })
+
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={handleToast} className=''>notify</button>
+      <ToastContainer />
     </div>
   )
 }
