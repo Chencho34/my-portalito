8c506290ffc5fd0c5976ebf29198169da99fc87a
Armando <armandocrescencio343@gmail.com>
2024-02-08 19:33:03 -0600

    MODIFIED: NavLogo component

--- src/components/nav/navLogo/NavLogo.tsx
+++ src/components/nav/navLogo/NavLogo.tsx
@@ -5,6 +5,6 @@ export default function NavLogo (): JSX.Element {
     <article className='flex items-center gap-4 text-[#fafafa] space-y-1.5'>
       <Link to='/' className='text-2xl font-medium tracking-wider leading-none'>MyPortalITO</Link>
       <p className='hidden lg:block text-sm font-thin'>System to students</p>
-    </article>
+    </article>  
   )
 }
