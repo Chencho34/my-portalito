74c22f65414545cd917183deb4360a28eb8c47dc
Armando <armandocrescencio343@gmail.com>
2024-02-09 01:45:35 -0600

    ADD: Modal component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,13 @@
 import Footer from './footer/Footer'
 import Input from './input/Input'
+import Modal from './modal/Modal'
 import Nav from './nav/Nav'
+import Wrapper from './wrapper/Wrapper'
 
 export {
-  Nav,
+  Wrapper,
   Footer,
-  Input  
+  Input, 
+  Modal,
+  Nav
 }
